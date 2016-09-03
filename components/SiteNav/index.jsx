abf727f97a3ad95d806d009684a1b4dedfda6901
Simon Reinsperger <reinsperger.smn@gmail.com>
2016-09-03 14:03:03 +0200

    Change of base config Indents several files with 2 spaces Restructures basic layout to mobile first approach Adds Gravatar Support Minor Styling (WIP)

--- components/SiteNav/index.jsx
+++ components/SiteNav/index.jsx
@@ -4,31 +4,31 @@ import { prefixLink } from 'gatsby-helpers'
 import './style.css'
 
 class SiteNav extends React.Component {
-    render() {
-        const {location} = this.props
-        return (
-            <nav className='blog-nav'>
-              <ul>
-                <li>
-                  <Link to="/" className={ location.pathname === prefixLink('/') ? "current" : null }> Articles
-                  </Link>
-                </li>
-                <li>
-                  <Link to="/about/" className={ location.pathname === prefixLink('/about/') ? "current" : null }> About me
-                  </Link>
-                </li>
-                <li>
-                  <Link to="/contact/" className={ location.pathname === prefixLink('/contact/') ? "current" : null }> Contact me
-                  </Link>
-                </li>
-              </ul>
-            </nav>
-            );
-    }
+  render() {
+    const {location} = this.props;
+    return (
+      <nav className='blog-nav'>
+        <ul>
+          <li>
+            <Link to="/" className={ location.pathname === prefixLink('/') ? "current" : null }> Articles
+            </Link>
+          </li>
+          <li>
+            <Link to="/about/" className={ location.pathname === prefixLink('/about/') ? "current" : null }> About me
+            </Link>
+          </li>
+          <li>
+            <Link to="/contact/" className={ location.pathname === prefixLink('/contact/') ? "current" : null }> Contact me
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    );
+  }
 }
 
 SiteNav.propTypes = {
-    location: React.PropTypes.object,
+  location: React.PropTypes.object,
 }
 
 export default SiteNav
--- components/SiteSidebar/index.jsx
+++ components/SiteSidebar/index.jsx
@@ -8,51 +8,57 @@ import './style.css'
 import profilePic from '../../pages/photo.jpg'
 
 class SiteSidebar extends React.Component {
-    render() {
-        const {location, children} = this.props
-        const isHome = location.pathname === prefixLink('/')
+  render() {
+    const {location, children} = this.props;
+    const isHome = location.pathname === prefixLink('/');
 
-        let header = (
-        <header>
-          <Link style={ {    textDecoration: 'none',    borderBottom: 'none',    outline: 'none'} } to={ prefixLink('/') }>
-          <img src={prefixLink(profilePic)} width='75' height='75' />
-          </Link>
-          { isHome ? (
-            <h1><Link style={ {    textDecoration: 'none',    borderBottom: 'none',    color: 'inherit'} } to={ prefixLink('/') }> { config.siteAuthor } </Link></h1>
-            ) :
-            <h2><Link style={ {    textDecoration: 'none',    borderBottom: 'none',    color: 'inherit'} } to={ prefixLink('/') }> { config.siteAuthor } </Link></h2> }
-          <p>
-            { config.siteDescr }
-          </p>
-        </header>
-        )
+    let header = (
+      <header>
+        <Link style={ {    textDecoration: 'none',    borderBottom: 'none',    outline: 'none'} } to={ prefixLink('/') }>
 
-        return (
-            <div className='sidebar'>
-              <div className='sidebar-inner'>
-                <div className='blog-details'>
-                  <header>
-                    { header }
-                  </header>
-                </div>
-                <div className='blog-options'>
-                  <SiteNav {...this.props}/>
-                  <footer>
-                    <SiteLinks {...this.props}/>
-                    <p className='copyright'>
-                      &copy; All rights reserved.
-                    </p>
-                  </footer>
-                </div>
-              </div>
-            </div>
-            );
-    }
+          { config.useGravatar ?
+            <img src={'https://s.gravatar.com/avatar/' + config.gravatarHash + '?s=300'} width='75' height='75' />
+            :
+            <img src={prefixLink(profilePic)} width='75' height='75' />
+          }
+
+        </Link>
+        { isHome ? (
+          <h1><Link style={ {    textDecoration: 'none',    borderBottom: 'none',    color: 'inherit'} } to={ prefixLink('/') }> { config.siteAuthor } </Link></h1>
+        ) :
+          <h2><Link style={ {    textDecoration: 'none',    borderBottom: 'none',    color: 'inherit'} } to={ prefixLink('/') }> { config.siteAuthor } </Link></h2> }
+        <p>
+          { config.siteDescr }
+        </p>
+      </header>
+    );
+
+    return (
+      <div className='sidebar'>
+        <div className='sidebar-inner'>
+          <div className='blog-details'>
+            <header>
+              { header }
+            </header>
+          </div>
+          <div className='blog-options'>
+            <SiteNav {...this.props}/>
+            <footer>
+              <SiteLinks {...this.props}/>
+              <p className='copyright'>
+                &copy; All rights reserved.
+              </p>
+            </footer>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 SiteSidebar.propTypes = {
-    children: React.PropTypes.any,
-    location: React.PropTypes.object,
-}
+  children: React.PropTypes.any,
+  location: React.PropTypes.object,
+};
 
 export default SiteSidebar
