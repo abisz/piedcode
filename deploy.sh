821db66e2c9fdda5147c4ecc26d0b352015c1d82
Simon Reinsperger <reinsperger.smn@gmail.com>
2016-10-01 23:43:08 +0200

    Adds Bash Script for automatically building and deploying Site

---
+++ deploy.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+FILE="./secret.sh"
+UPLOAD="public"
+
+if [ ! -f $FILE ]; then
+    echo "File with Server Credentials not found!"
+    exit
+fi
+
+. $FILE
+
+# Build
+gatsby build
+
+# Generate RSS/Atom Feed
+node scripts/generate-rss.js
+
+# Upload to Server
+lftp -u $USERNAME,$PASSWORD $SERVER <<END_SCRIPT
+mirror -R $UPLOAD "/"
+exit
+END_SCRIPT
+exit 0
